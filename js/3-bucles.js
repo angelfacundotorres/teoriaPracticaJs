abcc2c26571fbe21c565a402c2658e28b588ef06
angelfacundotorres <torresangelfacundo@gmail.com>
2023-05-31 10:31:42 -0300

    modificacion parte de bucles

---
+++ js/3-bucles.js
@@ -0,0 +1,35 @@
+//while
+/*while(condicion logica){
+todas las lineas de codigo que quiero que se repitan
+cambiar la condicion logica
+}
+*/ 
+// let contador = 1;
+// while (contador <= 100) {
+//     document.write('Elemento '+ contador + ' <br>');
+//     contador ++;
+// }
+
+//do while  hacer... mientras se cumpla la condicion logica (se ejecuta SI o SI una vez!!!)
+/* do{
+    todas las lineas de codigo a ejecutar
+} while(condicion logica)
+*/
+// let cont = 20;
+
+// do {
+//     document.write('Elemento '+ cont + ' <br>');
+//     cont--;
+// } while (cont >=1);
+
+//for funciona igual a un while
+
+/* for (inicializamos una variable; condicion logica; incrementar o decrementar la variable){
+    todas laslineas q quiero ejecutar
+}
+*/
+
+
+for (let i = 1; i <= 100; i++) {
+    document.write('Elemento '+ i + ' <br>');
+}
